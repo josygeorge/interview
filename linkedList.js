8025caaa2d563527c17c087d783156ade1da9e47
Josy George <joe.george.josy@gmail.com>
2023-09-24 22:37:08 -0400

    linked list creation and adding nodes

---
+++ linkedList.js
@@ -0,0 +1,50 @@
+//The Node class consists of a constructor that initializes the data and next property when creating a new Node.
+
+class Node {
+    constructor(data) {
+        this.data = data;
+        this.next = null;
+    }
+}
+
+
+//The LinkedList class has a constructor that initializes the head property when creating a new LinkedList.
+class LinkedList {
+    constructor() {
+        this.head = null;
+        this.tail = null;
+        this.length = 0;
+    }
+    // function to add data to linked list
+    add(data) {
+        const newNode = new Node(data);
+        if (!this.head) {
+            this.head = newNode;
+            this.tail = newNode;
+        } else {
+            this.tail.next = newNode;
+            this.tail = newNode;
+        }
+        this.length++;
+        return this;
+    }
+    //print the linked list
+    printLinkedList() {
+        let current = this.head;
+        while (current) {
+            console.log(current.data);
+            current = current.next;
+        }
+    }
+}
+
+const list = new LinkedList();
+list.add("node1");
+list.add("node2");
+list.add("node3");
+list.add("node4");
+console.log("Initial List:");
+list.printLinkedList();
+
+
+
