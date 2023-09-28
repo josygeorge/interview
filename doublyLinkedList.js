303f84a8a30ea3ef661f5c8bbfe5833db07ecdc7
Josy George <joe.george.josy@gmail.com>
2023-09-28 19:45:05 -0400

    doubly-linked-list-creation-correction

--- doublyLinkedList.js
+++ doublyLinkedList.js
@@ -8,38 +8,40 @@ function createNode(value) {
 
 // main class
 class DoublyLinkedList {
-    constructor() {
-        this.head = null;
-        this.tail = null;
+    constructor(value) {
+        this.head = {
+            value: value,
+            next: null,
+            previous: null,
+        };
+        this.tail = this.head;
         this.size = 0;
     }
-    // inserting
-    insert(value) {
+    // inserting at the end of the list
+    insertAtEnd(value) {
         this.size++;
         let newNode = createNode(value);
         if (this.tail) {
             this.tail.next = newNode;
             newNode.previous = this.tail;
             this.tail = newNode;
-            return newNode;
         }
-        this.head = this.tail = newNode;
-        return newNode;
+        this.printList();
     }
-    print() {
+    printList() {
         document.write("The Elements in the Doubly Linked List are :</br> ");
         let current = this.head;
         while (current) {
             document.write(
-                `${current.previous?.value}   ${current.value}   ${current.next?.value}`
+                `${current.value}<br>`
             );
             current = current.next;
         }
     }
 }
 
-let dLinkedList = new DoublyLinkedList();
-dLinkedList.insert(7);
-dLinkedList.insert(8);
-dLinkedList.insert(9);
-dLinkedList.print();
+let dLinkedList = new DoublyLinkedList(11);
+dLinkedList.insertAtEnd(7);
+dLinkedList.insertAtEnd(8);
+dLinkedList.insertAtEnd(9);
+dLinkedList.printList();
