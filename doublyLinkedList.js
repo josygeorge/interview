b3301f368394076f49db898dab253982de247e3b
Josy George <joe.george.josy@gmail.com>
2023-09-28 20:43:14 -0400

    doubly-linked-list-remove-at-an-Index

--- doublyLinkedList.js
+++ doublyLinkedList.js
@@ -38,8 +38,91 @@ class DoublyLinkedList {
         }
         this.printList();
     }
+
+    // Insert node at a given index
+    insert(index, value) {
+        if (!Number.isInteger(index) || index < 0 || index > this.length + 1) {
+            console.log(`Invalid index. Current length is ${this.length}.`);
+            return this;
+        }
+
+        // If index is 0, prepend
+        if (index === 0) {
+            this.prepend(value);
+            return this;
+        }
+
+        // If index is equal to this.length, append
+        if (index === this.length) {
+            this.append(value);
+            return this;
+        }
+
+        // Reach the node at that index
+        let newNode = new Node(value);
+        let previousNode = this.head;
+
+        for (let k = 0; k < index - 1; k++) {
+            previousNode = previousNode.next;
+        }
+
+        let nextNode = previousNode.next;
+
+        newNode.next = nextNode;
+        previousNode.next = newNode;
+        newNode.previous = previousNode;
+        nextNode.previous = newNode;
+
+        this.length++;
+        this.printList();
+    }
+
+    // Remove a node
+    removeAtIndex(index) {
+        if (!Number.isInteger(index) || index < 0 || index > this.length) {
+            console.log(`Invalid index. Current length is ${this.length}.`);
+            return this;
+        }
+
+        // Remove head
+        if (index === 0) {
+            this.head = this.head.next;
+            this.head.previous = null;
+
+            this.length--;
+            this.printList();
+            return this;
+        }
+
+        // Remove tail
+        if (index === this.length - 1) {
+            this.tail = this.tail.previous;
+            this.tail.next = null;
+
+            this.length--;
+            this.printList();
+            return this;
+        }
+
+        // Remove node at an index
+        let previousNode = this.head;
+
+        for (let k = 0; k < index - 1; k++) {
+            previousNode = previousNode.next;
+        }
+        let deleteNode = previousNode.next;
+        let nextNode = deleteNode.next;
+
+        previousNode.next = nextNode;
+        nextNode.previous = previousNode;
+
+        this.length--;
+        this.printList();
+        return this;
+    }
+
     printList() {
-        document.write("The Elements in the Doubly Linked List are :</br> ");
+        document.write("Doubly Linked List is now :</br> ");
         let current = this.head;
         while (current) {
             document.write(
@@ -48,6 +131,8 @@ class DoublyLinkedList {
             current = current.next;
         }
     }
+
+
 }
 document.write("DOUBLY LINKED LIST <br>");
 let dLinkedList = new DoublyLinkedList(6);
@@ -60,4 +145,7 @@ document.write("****** Insert at Front:<br>");
 dLinkedList.insertAtFront(22);
 dLinkedList.insertAtFront(33);
 dLinkedList.insertAtFront(44);
+document.write("****** Remove at an index; eg: 2nd index:<br>");
+dLinkedList.removeAtIndex(2);
+
 dLinkedList.printList();
