7ee4eae5c85951b03b33842eabf11ca89f1e3abe
Josy George <joe.george.josy@gmail.com>
2023-09-29 20:44:42 -0400

    closure-eg.

---
+++ closure.js
@@ -0,0 +1,16 @@
+//Closure test
+/*write a function which will output something like this:
+
+var addSix = createBase(6);
+addSix(10); // returns 16
+addSix(21); // returns 27
+*/
+function createBase(baseNum) {
+    function inner(n) {
+        return baseNum + n;
+    }
+    return inner;
+}
+var addSix = createBase(6);
+console.log(addSix(10)); // returns 16
+console.log(addSix(21)); // returns 27
