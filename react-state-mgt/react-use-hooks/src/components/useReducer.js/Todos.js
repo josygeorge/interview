2b728fd4c4dc932ca65f7a4b43de695d3762a08c
Josy George <joe.george.josy@gmail.com>
2023-10-01 15:40:29 -0400

    usereducerhook eg.-reducer-function-addtask

--- react-state-mgt/react-use-hooks/src/components/useReducer.js/Todos.js
+++ react-state-mgt/react-use-hooks/src/components/useReducer.js/Todos.js
@@ -9,16 +9,33 @@ const initialState = []
 function reducer(state, action) {
     switch (action.type) {
         case 'ADD_TASK':
-            console.log('ADD');
-            break;
+            return [...state, {
+                id: state.length + 1,
+                name: action.payload
+            }]
         default: return state;
     }
 }
 
 const Todos = () => {
-    const [state, dispatch] = useReducer(reducer, initialState)
+    const [todos, dispatch] = useReducer(reducer, initialState)
     return (
-        <div>Todo app</div>
+        <div>
+            <h1>Todo app</h1>
+            <h4>Todo list {todos.length}</h4>
+            Add new task:
+            <input
+                type="text"
+                onBlur={
+                    (e) =>
+                        dispatch(
+                            { type: 'ADD_TASK', payload: e.target.value }
+                        )
+                }
+            />
+            <hr />
+            {todos.map(todo => <li key={todo.id}>{todo.name}</li>)}
+        </div>
     )
 }
 
