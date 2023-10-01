334a9fcd9b62d9ac6ae9af4df57084e021d1eb9f
Josy George <joe.george.josy@gmail.com>
2023-10-01 15:14:27 -0400

    usereducer eg.-setup

---
+++ react-state-mgt/react-use-hooks/src/App.js
@@ -0,0 +1,11 @@
+import Todo from "./components/useReducer.js/todo";
+
+function App() {
+  return (
+    <div className="App">
+      <Todo />
+    </div>
+  );
+}
+
+export default App;
---
+++ react-state-mgt/react-use-hooks/src/components/useReducer.js/todo.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const todo = () => {
+    return (
+        <div>todo app</div>
+    )
+}
+
+export default todo
