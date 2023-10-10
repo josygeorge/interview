10c508f4b5dda1f5493906036232793ca7f5ac08
Josy George <joe.george.josy@gmail.com>
2023-10-10 00:43:18 -0400

    get-val-at-given-path-of-obj-2nd

--- getValAtGivenPathOfObject.js
+++ getValAtGivenPathOfObject.js
@@ -4,5 +4,23 @@ const getVal = (path, obj) => path.split('.').reduce(
     (acc, c) => acc && acc[c], obj
 )
 
+//
+
+function deepFind(path, obj) {
+    var paths = path.split('.')
+        , current = obj
+        , i;
+
+    for (i = 0; i < paths.length; ++i) {
+        if (current[paths[i]] == undefined) {
+            return undefined;
+        } else {
+            current = current[paths[i]];
+        }
+    }
+    return current;
+}
+
 // get value
 console.log(getVal('a.c', { a: { b: 2, c: 'Hello World' } }))
+console.log(deepFind('a.c', { a: { b: 2, c: 'Hello World' } }))
