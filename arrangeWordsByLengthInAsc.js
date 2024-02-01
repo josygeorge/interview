97881287b3fc326cf7e0bbc1afbbdd43e6be4fbe
Josy George <joe.george.josy@gmail.com>
2024-01-31 20:35:38 -0500

    arrangewords js

---
+++ arrangeWordsByLengthInAsc.js
@@ -0,0 +1,16 @@
+// arranging words by their length in a sentence in Ascending Order
+
+const str = 'react is a javascript library to build scalable applications';
+
+// output : a is to react build librabry scalable javascript applications
+
+//Approach
+// split the str to arr
+// sort the arr
+// join the arr to str
+
+function arrangeString(str) {
+    //return str.split(" ");
+    return str.split(" ").sort((a, b) => a.length - b.length).join(" ");
+}
+console.log(arrangeString(str));
